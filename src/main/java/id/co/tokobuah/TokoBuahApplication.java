f9f2aef1c298ff70041598a5dff9b2ba92daf70a
student <nurawartami@gmail.com>
2024-04-01 15:31:37 +0700

    first

---
+++ src/main/java/id/co/tokobuah/TokoBuahApplication.java
@@ -0,0 +1,13 @@
+package id.co.tokobuah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoBuahApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokoBuahApplication.class, args);
+    }
+
+}
